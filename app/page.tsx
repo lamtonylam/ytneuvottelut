02cce20fec13d0d23aa3c484647ef58e910d701b
Tony Lam <tony.lam@iki.fi>
2025-08-22 17:42:42 +0200

    Feat: add footer

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,7 @@ export default function Home() {
   const chartData = getChartData(layoffs);
 
   return (
+    <>
       <main
         style={{
           padding: '2rem',
@@ -27,23 +28,37 @@ export default function Home() {
           minHeight: '100vh',
         }}
       >
-      <h1
+        <h1
+          style={{
+            fontFamily: 'Roboto, Arial, sans-serif',
+            fontSize: '2rem',
+            fontWeight: 'bold',
+            marginBottom: '1rem',
+            textAlign: 'center',
+          }}
+        >
+          Finnish Tech Layoffs Tracker
+        </h1>
+        {layoffs.length > 0 && (
+          <>
+            <LayoffsChart data={chartData} options={chartOptions} />
+            <LayoffsTable layoffs={layoffs} />
+          </>
+        )}
+      </main>
+      <footer
         style={{
           fontFamily: 'Roboto, Arial, sans-serif',
-          fontSize: '2rem',
-          fontWeight: 'bold',
-          marginBottom: '1rem',
-          textAlign: 'center',
+          padding: '2rem',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
         }}
       >
-        Finnish Tech Layoffs Tracker
-      </h1>
-      {layoffs.length > 0 && (
-        <>
-          <LayoffsChart data={chartData} options={chartOptions} />
-          <LayoffsTable layoffs={layoffs} />
-        </>
-      )}
-    </main>
+        <p>Have a tip / layoff missing?</p>
+        <a href="mailto:layoffs@ynot.fi">layoffs@ynot.fi</a>
+      </footer>
+    </>
   );
 }
